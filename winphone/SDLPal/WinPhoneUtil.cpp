325cac7a6b4c88629ad687ac3c77fd1a5dfefa1e
Pal Lockheart <ex@palx.org>
2016-02-13 23:06:30 +0800

    Emulate C File APIs with WinRT Async APIs

--- winphone/SDLPal/WinPhoneUtil.cpp
+++ winphone/SDLPal/WinPhoneUtil.cpp
@@ -2,19 +2,13 @@
 #include <string>
 #include <DXGI.h>
 #include <ppltasks.h>
+#include <unordered_map>
+#include "AsyncHelper.h"
 
 #define PAL_PATH_NAME	"SDLPAL"
 
 static std::string g_savepath, g_basepath;
-
-template<typename T>
-static bool WaitOnTask(T task, int64 wait_timeout)
-{
-	auto start = GetTickCount64();
-	while (!task.is_done() && (int64)(GetTickCount64() - start) <= wait_timeout)
-		Sleep(1);
-	return task.is_done();
-}
+static Windows::Storage::StorageFolder^ g_root;
 
 static void ConvertString(Platform::String^ src, std::string& dst)
 {
@@ -25,8 +19,6 @@ static void ConvertString(Platform::String^ src, std::string& dst)
 
 static bool CheckGamePath(Windows::Storage::StorageFolder^ root)
 {
-	std::string msg;
-	char xmsg[256];
 	Platform::String^ required_files[] = {
 		L"ABC.MKF", L"BALL.MKF", L"DATA.MKF", L"F.MKF", L"FBP.MKF",
 		L"FIRE.MKF", L"GOP.MKF", L"MAP.MKF", L"MGO.MKF", L"PAT.MKF",
@@ -37,41 +29,32 @@ static bool CheckGamePath(Windows::Storage::StorageFolder^ root)
 	};
 	/* The words.dat & m.msg may be configurable in the future, so not check here */
 
-	/* Try to get the path */
-	auto foldertask = concurrency::create_task(root->GetFolderAsync(PAL_PATH_NAME));
-	if (!WaitOnTask(foldertask, 500)) return false;		// Wait for 500ms max
-
 	try
 	{
 		/* Check the access right of necessary files */
-		auto folder = foldertask.get();
+		auto folder = AWait(root->GetFolderAsync(PAL_PATH_NAME));
 		for (int i = 0; i < 13; i++)
 		{
-			auto filetask = concurrency::create_task(foldertask.get()->GetFileAsync(required_files[i]));
-			if (!WaitOnTask(filetask, 500) || _waccess_s(filetask.get()->Path->Begin(), 4)) {
-				ConvertString(filetask.get()->Path, msg);
-				sprintf(xmsg, "file:%s, err:%s", msg.c_str(), strerror(errno));
+			try {
+				auto file = AWait(folder->GetFileAsync(required_files[i]));
 			}
-			else {
-				ConvertString(filetask.get()->Path, msg);
-				sprintf(xmsg, "file:%s, err:%s", msg.c_str(), strerror(errno));
-				return true;
+			catch (Platform::Exception^ e) {
+				return false;
 			}
 		}
 		for (int i = 0; i < 2; i++)
 		{
-			auto filetask = concurrency::create_task(foldertask.get()->GetFileAsync(optional_required_files[i]));
-			try { if (WaitOnTask(filetask, 500) && _waccess_s(filetask.get()->Path->Begin(), 2) == 0)
-				return true; }
-			catch(Platform::Exception^) {}
+			try
+			{
+				auto filetask = AWait(folder->GetFileAsync(optional_required_files[i]));
+				g_root = folder;
+				return true;
+			}
+			catch (Platform::Exception^ e) {}
 		}
 	}
 	catch(Platform::Exception^ e)
-	{ /* Accessing SD card failed, or required file is missing, or access is denied */ 
-		std::string errMsg;
-		ConvertString(e->Message, errMsg);
-		printf("Access Error:%s\n", errMsg.c_str());
-	}
+	{ /* Accessing SD card failed, or required file is missing, or access is denied */ 	}
 	return false;
 }
 
@@ -80,38 +63,27 @@ LPCSTR UTIL_BasePath(VOID)
 {
 	if (g_basepath.empty())
 	{
-		auto enumtask = concurrency::create_task(Windows::Storage::KnownFolders::PicturesLibrary->GetFoldersAsync());
-		if (WaitOnTask(enumtask, 1000))		// Wait for 1000ms max
+		auto folderiter = AWait(Windows::Storage::KnownFolders::PicturesLibrary->GetFoldersAsync())->First();
+		while (folderiter->HasCurrent)
 		{
-			auto folderiter = enumtask.get()->First();
-			while (folderiter->HasCurrent)
+			if (CheckGamePath(folderiter->Current))
 			{
-				if (CheckGamePath(folderiter->Current))
-				{
-					/* Folder examination succeeded */
-					auto folder = folderiter->Current->Path;
-					if (folder->End()[-1] != L'\\') folder += "\\";
-					folder += PAL_PATH_NAME "\\";
-					ConvertString(folder, g_basepath);
+				/* Folder examination succeeded */
+				auto folder = folderiter->Current->Path;
+				if (folder->End()[-1] != L'\\') folder += "\\";
+				folder += PAL_PATH_NAME "\\";
+				ConvertString(folder, g_basepath);
 
-					/* Check whether the folder is writable */
-					FILE* fp;
-					if (_wfopen_s(&fp, (folder + "sdlpal.rpg")->Begin(), L"w") == 0)
-					{
-						g_savepath = g_basepath;
-						fclose(fp);
-					}
-					break;
-				}
-				folderiter->MoveNext();
+				g_savepath = g_basepath;
+				break;
 			}
+			folderiter->MoveNext();
 		}
 
 		if (g_basepath.empty())
 		{
 			auto path = Windows::ApplicationModel::Package::Current->InstalledLocation->Path+"\\Assets\\Data\\";
 			ConvertString(path, g_basepath);
-			//g_basepath.assign("Assets\\Data\\");
 		}
 	}
 	return g_basepath.c_str();
@@ -159,4 +131,110 @@ UTIL_WP_GetScreenSize_exit:
 	if (pFactory) pFactory->Release();
 
 	return retval;
+}
+
+extern "C" {
+	std::hash<std::string> hasher;
+	std::unordered_map<FILE*, std::wstring> fileMap;
+	std::unordered_map<FILE*, long> fileOffsetMap;
+	char outputStr[512];
+
+	auto openFile(const std::wstring &wid_str) {
+		std::wstring last = wid_str.substr(wid_str.find_last_of(L'\\') + 1, wid_str.length());
+		const wchar_t* w_filename = last.c_str();
+		Platform::String ^nFilename = ref new Platform::String(w_filename);
+		auto storageFile = AWait(g_root->GetFileAsync(nFilename));
+		return storageFile;
+	}
+	long file_length(FILE *fp) {
+		auto storageFile = openFile(fileMap[fp]);
+		auto basicInfo = AWait(storageFile->GetBasicPropertiesAsync());
+		return basicInfo->Size;
+	}
+
+	FILE *fopen_uwp(const char *filename, const char *mode) {
+		size_t newsize = strlen(filename) + 1;
+		wchar_t * wcstring = new wchar_t[newsize];
+		size_t convertedChars = 0;
+		setlocale(LC_ALL, "chs");
+		mbstowcs_s(&convertedChars, wcstring, newsize, filename, _TRUNCATE);
+		std::wstring wid_str = std::wstring(wcstring);
+		auto storageFile = openFile(wid_str);
+		FILE *result = reinterpret_cast<FILE*>(hasher(filename));
+		fileMap[result] = wid_str;
+		fileOffsetMap[result] = 0;
+		return result;
+	}
+	long ftell_uwp(FILE *fp) {
+		auto storageFile = openFile(fileMap[fp]);
+		return fileOffsetMap[fp];
+	}
+	int fseek_uwp(FILE *fp, long offset, int whence) {
+		auto storageFile = openFile(fileMap[fp]);
+		long length = file_length(fp);
+		switch (whence) {
+		case SEEK_CUR:
+				offset += ftell_uwp(fp);
+				break;
+		case SEEK_END:
+				offset += length;
+				break;
+		}
+		offset = min(max(0,offset), length);
+		fileOffsetMap[fp] = offset;
+		return offset;
+	}
+	void rewind_uwp(FILE *fp) {
+		fseek_uwp(fp,0,SEEK_SET);
+	}
+	char *fgets_uwp(char *ptr, size_t length, FILE *fp) {
+		auto storageFile = openFile(fileMap[fp]);
+		auto origOffset = fileOffsetMap[fp];
+		auto texts = AWait(Windows::Storage::FileIO::ReadLinesAsync(storageFile));
+		std::string msg;
+		bool eof;
+		long offset = min(texts->Size-1, origOffset);
+		ConvertString(texts->GetView()->GetAt(offset), msg);
+		fileOffsetMap[fp] = offset+1;
+		eof = (offset >= texts->Size-1);
+		if( !eof )
+			strcpy(ptr, msg.c_str());
+		return eof ? nullptr : ptr;
+	}
+	size_t fread_uwp(void *ptr, size_t size, size_t nitems, FILE *fp) {
+		auto length = size * nitems;
+		auto storageFile = openFile(fileMap[fp]);
+		auto origOffset = ftell_uwp(fp);
+		auto buffer = AWait(Windows::Storage::FileIO::ReadBufferAsync(storageFile));
+		auto dataReader = Windows::Storage::Streams::DataReader::FromBuffer(buffer);
+		auto bytesSkip = ref new Platform::Array<uint8>(origOffset);
+		dataReader->ReadBytes(bytesSkip);
+		auto origLength = length;
+		length = min(length, dataReader->UnconsumedBufferLength);
+		auto bytes = ref new Platform::Array<uint8>(length);
+		dataReader->ReadBytes(bytes);
+		std::copy(bytes->begin(), bytes->end(), (uint8*)ptr);
+		fileOffsetMap[fp] = origOffset + length;
+		return length;
+	}
+	int fputs_uwp(const char *ptr, size_t &length, FILE *fp) {
+		auto storageFile = openFile(fileMap[fp]);
+		auto file = AWait(storageFile->OpenAsync(Windows::Storage::FileAccessMode::ReadWrite));
+		auto fileStream = file->GetOutputStreamAt(ftell_uwp(fp));
+		auto dataWriter = ref new Windows::Storage::Streams::DataWriter(fileStream);
+		length = min(length, dataWriter->UnstoredBufferLength);
+		auto bytes = ref new Platform::Array<uint8>(length);
+		std::copy(ptr, ptr + length, bytes->begin());
+		dataWriter->WriteBytes(bytes);
+		return length;
+	}
+	size_t fwrite_uwp(const void *ptr, size_t size, size_t nitems, FILE *fp) {
+		auto length = size * nitems;
+		fputs_uwp((const char*)ptr, length, fp);
+		return length;
+	}
+	int fclose_uwp(FILE *fp) {
+		auto storageFile = openFile(fileMap[fp]);
+		return 0;
+	}
 }
